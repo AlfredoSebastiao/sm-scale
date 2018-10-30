c517058fd7af262318902b2a147d8bf9cfb1d866
alfredosebastiao <stiaoalfredo@gmail.com>
2018-10-30 22:38:35 +0100

    Update app/EscalaDeLeitura.php

--- app/EscalaDeLeitura.php
+++ app/EscalaDeLeitura.php
@@ -8,9 +8,13 @@ use Carbon\Carbon;
 class EscalaDeLeitura extends Model
 {
     protected $table = 'escala_de_leituras';
-
+    protected $with = ['primeiraLeitura'];
     public function getDataAttribute($date)
     {
         return Carbon::parse($date);
+    }
+
+    public function primeiraLeitura(){
+        return $this->hasMany('App\FuncaoDoMembro', 'primeira_portugues_id');
     }
 }
